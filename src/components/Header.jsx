a208861ecc37bb60c313b06a7d5652dc2c5167ba
Khadidja <khadija.aitsiali@gmail.com>
2025-06-09 00:55:02 +0200

    add social media links - routes done - work on landing page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,34 +8,37 @@ const Header = () => {
       <nav className='navbar_menu'>
         <li>
           <Link to="/">
-            le hall 
-                    <svg fill="#2500E5" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M7.8 10a2.2 2.2 0 0 0 4.4 0 2.2 2.2 0 0 0-4.4 0z"></path></g></svg>
-
+            useless hall 
           </Link>
         </li>
+        <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="#1D2D93" class="bi bi-dot" stroke="#1D2D93"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M8 9.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z"></path> </g></svg>
         <li>
           <Link to="/ne_clique_pas">
-            ne clique surtout pas
+            do not click
           </Link>
         </li>
+        <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="#1D2D93" class="bi bi-dot" stroke="#1D2D93"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M8 9.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z"></path> </g></svg>
         <li>
           <Link to="/machine_a_compliments">
-            la machine à compliments
+            ego booster
           </Link>
         </li>
+        <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="#1D2D93" class="bi bi-dot" stroke="#1D2D93"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M8 9.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z"></path> </g></svg>
         <li>
           <Link to="/qui_s_ennuie">
-            la page qui s'ennuie de fou
+            bored pixels
           </Link>
         </li>
+        <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="#1D2D93" class="bi bi-dot" stroke="#1D2D93"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M8 9.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z"></path> </g></svg>
         <li>
           <Link to="/rage_button">
-            le bouton qui rage frère
+            instant regret
           </Link>
         </li>
+        <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="#1D2D93" class="bi bi-dot" stroke="#1D2D93"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M8 9.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z"></path> </g></svg>
         <li>
           <Link to="/">
-            pas encore pas encore
+            no idea
           </Link>
         </li>
       </nav>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,40 @@
-import React from 'react';
-import Header from './Header';
 import './../styles/main.css';
+import'./../styles/home.css';
+import { Link } from 'react-router-dom';
+import { FaGithub, FaCode, FaLinkedin } from 'react-icons/fa';
+import { RiArrowDropRightLine } from "react-icons/ri";
 
 const Home = () => {
+
   return (
-    <>
-      <Header/>
-      
-    </>
+    <section className='home_container'>
+
+      <div className="social_media">
+        <a href="https://khadija-asa.github.io/web_developer/" target="_blank" rel="noopener noreferrer">
+          <FaCode size={20} />
+        </a>
+        <a href="https://github.com/Khadija-Asa" target="_blank" rel="noopener noreferrer">
+        <FaGithub size={20} />
+        </a>
+        <a href="https://www.linkedin.com/in/khadidja-ait-si-ali/" target="_blank" rel="noopener noreferrer">
+          <FaLinkedin size={20} />
+        </a>
+      </div>
+
+      <p>Background perfectly uncentered. Just how we like it.</p>
+      <h1>
+        welcome to the museum <br />
+        of <i>useless</i>&nbsp; things
+      </h1>
+      <p>
+        Where function takes a vacation and pixels misbehave. <br />
+       Where logic naps and purpose politely left the room.
+      </p>
+      <Link to="/ne_clique_pas">
+        <button className='btn_enter'>ENTER <RiArrowDropRightLine />
+</button>
+      </Link>
+    </section>
   )
 }
 
